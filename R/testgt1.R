4f68b4bb59296bfaceddce85ffcb88261324e68c
Roland Hansson <rolandhhansson@gmail.com>
2016-01-19 10:58:57 -0800

    edit

--- R/testgt1.R
+++ R/testgt1.R
@@ -32,8 +32,13 @@ testgt1 <- function (sw, ne, zoom=14, theme="starb", style="val", theme2="") {
 getPoints <-function(theme, xmin, ymin, xmax, ymax) {
   # set working dir to path of R script for csv file input
   # http://stackoverflow.com/questions/3452086/getting-path-of-an-r-script?lq=1
-  rdir <- getSrcDirectory(function(x) {x})
-  setwd(rdir)
+  tryCatch({
+    rdir <- getSrcDirectory(function(x) {x})
+  }, error = function(err) {      
+    rdir <- getwd()  # opencpu ERROR: character argument expected In call: setwd(rdir)
+  }, finally = {
+    setwd(rdir)      
+  })
   df2 <- data.frame(matrix(ncol = 0, nrow = 0))
   if (theme=="starb") {
     df = read.csv("starbucks_oc.csv", sep="\t")
